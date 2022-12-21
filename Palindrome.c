d6dc3771e6aa6a9802352a30ca772030ad4a9287
Jimmy-Rais <raisgachaba@gmail.com>
2022-12-21 21:02:58 +0530

    #include<stdio.h> int main() {     int n,r=0,s=0,m;     scanf("%d",&n);     m=n;     while(n>0)     {         r=r*10+n%10;         n=n/10;     }     if(r==m)     {         printf("True");     }     else     {         printf("False");     } }

--- Palindrome.c
+++ Palindrome.c
@@ -1,18 +1,20 @@
-#include<stdio.h>  
-int main()    
-{    
-int n,r,sum=0,temp;  
-scanf("%d",&n);    
-temp=n;    
-while(n>0)    
-{    
-r=n%10;    
-sum=(sum*10)+r;    
-n=n/10;    
-}    
-if(temp==sum)    
-printf("Palindrome");    
-else    
-printf("Not Palindrome");   
-return 0;  
+#include<stdio.h>
+int main()
+{
+    int n,r=0,s=0,m;
+    scanf("%d",&n);
+    m=n;
+    while(n>0)
+    {
+        r=r*10+n%10;
+        n=n/10;
+    }
+    if(r==m)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
 }
